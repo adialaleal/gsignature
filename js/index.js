f6e36cf15ec41ceda4fa529f561b81c4abd10d0b
Lucas Leal <lealadiala@gmail.com>
2019-01-04 15:32:16 -0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,20 @@
+function reviewFunction(){
+		if (document.getElementById('review').checked) { 
+      document.getElementById('name').innerHTML = nm.value;
+      document.getElementById('position').innerHTML = pos.value;
+      document.getElementById('email').innerHTML = em.value;
+      document.getElementById('tel').innerHTML = mb.value;
+      document.getElementById('skype').innerHTML = sk.value;
+      document.getElementById('linkedin').innerHTML = lk.value;
+      }
+}
+
+function getFunction(){
+		if (document.getElementById('get').checked) { 
+		var e=document.getElementById("htmlcode");
+      var content=e.innerHTML;
+      
+         
+ 		}
+ 	
+		}

7463b0d2d48c43c5dd5d5cba863c7bb555caef1f
Lucas Leal <lealadiala@gmail.com>
2019-01-08 11:38:30 -0200

    Alteração na mudança das funções de links dinâmicos.

--- js/index.js
+++ js/index.js
@@ -25,21 +25,25 @@ function getFunction(){
   });
 })
 
-function addskype(){
-var skypeid = document.getElementById("sk");
-var s = skypeid.value;
-document.getElementById('skype').innerHTML= s;
-document.getElementById('skype').href="skype:"+s+"?userinfo";
-}
-function addphone(){
-var phoneid = document.getElementById("mb");
-var s = phoneid.value;
-document.getElementById('tel').innerHTML= s;
-document.getElementById('tel').href="callto:"+s;
-}
-function addskype(){
-var skypeid = document.getElementById("sk");
-var s = skypeid.value;
-document.getElementById('skype').innerHTML= s;
-document.getElementById('skype').href="skype:"+s+"?userinfo";
+function addlinks(){
+  //Add Skype dinamic link.
+  var skypeid = document.getElementById("sk");
+  var s = skypeid.value;
+  document.getElementById('skype').innerHTML= s;
+  document.getElementById('skype').href="skype:"+s+"?userinfo";
+//Add Phone dinamic link.
+  var phoneid = document.getElementById("mb");
+  var s = phoneid.value;
+  document.getElementById('tel').innerHTML= s;
+  document.getElementById('tel').href="callto:"+s;
+//Add LinkedIn dinamic link.
+  var linkedinid = document.getElementById("lk");
+  var s = skypeid.value;
+  document.getElementById('linkedin').innerHTML= s;
+  document.getElementById('linkedin').href="https://linkedin.com/in/"+s;
+//Add Email dinamic link.
+  var emailid = document.getElementById("em");
+  var s = emailid.value;
+  document.getElementById('email').innerHTML= s;
+  document.getElementById('email').href="mailto:"+s;
 }
